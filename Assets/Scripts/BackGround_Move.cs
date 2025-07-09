c5d4170b6ccb3d80cb3cb9ff7a2f70c04589031c
woosung03 <woosungkang2003@gmial.com>
2025-07-09 16:25:07 +0900

    BackGround 무한 스크롤 구현

---
+++ Assets/Scripts/BackGround_Move.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BackGround_Move : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+
+    public float speed;        // 초기 속도
+    public float acceleration = 0.01f; // 시간당 증가 속도
+    public float maxSpeed = 1.0f;      // 최대 속도 제한
+
+    private float offsetX = 0f;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        speed = Mathf.Min(speed, maxSpeed);     // 최대 속도 제한
+
+        speed += acceleration * Time.deltaTime; // 매 프레임 속도 증가
+        offsetX += speed * Time.deltaTime;      // 이동 거리 계산
+        meshRenderer.material.mainTextureOffset = new Vector2(offsetX, 0);
+    }
+}
