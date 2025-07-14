c99011a65b1b29005e70c2aa7c07c93a64d7758b
woosung03 <woosungkang2003@gmial.com>
2025-07-14 16:44:50 +0900

    메인메뉴 UI 테스트

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickGameStart()
+    {
+        // Load the game scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameStart");
+    }
+
+    public void OnClickExit()
+    {
+        // Exit the application
+        Application.Quit();
+
+        // If running in the editor, stop playing
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
