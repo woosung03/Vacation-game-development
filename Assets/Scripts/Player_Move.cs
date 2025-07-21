6c147ac0895bbbc1aadfc76680f3cfdb334311b7
woosung03 <woosungkang2003@gmial.com>
2025-07-21 16:57:17 +0900

    장애물 프리펩 추가

--- Assets/Scripts/Player_Move.cs
+++ Assets/Scripts/Player_Move.cs
@@ -6,8 +6,8 @@ public class Player_J : MonoBehaviour
 
     public float JumpForce = 5f;
 
-    private int jumpCount = 0;         // ���� ���� Ƚ��
-    public int maxJumpCount = 2;       // �ִ� ���� ���� Ƚ��
+    private int jumpCount = 0;         //   현재 점프 횟수
+    public int maxJumpCount = 2;       //   최대 점프 가능 횟수
 
     void Start()
     {
@@ -19,7 +19,7 @@ public class Player_J : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumpCount)
         {
-            PlayerRB.linearVelocity = new Vector2(PlayerRB.linearVelocity.x, 0f); // ���� y�ӵ� ����
+            PlayerRB.linearVelocity = new Vector2(PlayerRB.linearVelocity.x, 0f); // 캐릭터 y속도 제거
             PlayerRB.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
             jumpCount++;
         }
@@ -29,7 +29,7 @@ public class Player_J : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            jumpCount = 0; // ���� ������ ���� Ƚ�� �ʱ�ȭ
+            jumpCount = 0; // 땅에 닿으면 점프 횟수 초기화
         }
     }
 }
